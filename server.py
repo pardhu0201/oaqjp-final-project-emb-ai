d99c780f45bdfe8da75f1c23362d4c0c7e6b6815
pardhu0201 <pardhu.b23@iiits.in>
2026-07-18 05:26:39 -0400

    Improve server.py for pylint

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ def emotion_detection():
 
     response = emotion_detector(text_to_analyze)
 
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
+
     return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
